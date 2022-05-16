8d49899994be4a9548dfb622713c76a0991fa216
ahijevyc <ahijevyc@ucar.edu>
2022-05-16 13:26:32 -0600

    "sounding list" to "LTC category"
    put obs and narr in same DataFrame under varx and vary
    
    LTC category by style
    place by hue

--- scripts/NARR_obs_scatter.py
+++ scripts/NARR_obs_scatter.py
@@ -50,37 +50,54 @@ def main():
         logging.error(f"choices {df.columns}")
         sys.exit(1)
 
-    # remove dirname and extension from sounding list column.
-    df["sounding list"] = [os.path.basename(os.path.splitext(slist)[0]).replace(".","\n") for slist in df["sounding list"]]
+    df = df.rename(columns={"sounding list":"LTC category"})
+    # remove dirname and extension from category column.
+    df["LTC category"] = [os.path.basename(os.path.splitext(slist)[0]).replace(".","\n") for slist in df["LTC category"]]
 
     # This is kind of kludgy, but better than before
     obs  = df[df["sounding type"] == "obs"].reset_index()
     narr = df[df["sounding type"] == "NARR"].reset_index()
-    obs[vary] = narr[vary] # Replace vary in obs with vary in narr
+    narrvalues = narr[vary]
+    if varx == vary:
+        # differentiate column names
+        vary = "narr "+vary
+    obs[vary] = narrvalues # Replace vary in obs with narr values
     # Then plot obs
-    ax = sns.scatterplot(data=obs, x=varx, y=vary, hue="place", style="sounding list")
+    blue, orange, green, red, purple, brown, pink, gray, yellow,teal = sns.color_palette()
+    colors = dict(upshr=teal, dnshr=red)
+    ax = sns.scatterplot(data=obs, x=varx, y=vary, hue="place", palette=colors, style="LTC category")
     plt.setp(ax.get_legend().get_texts(), fontsize='6') # for legend text
-    plt.setp(ax.get_legend().get_title(), fontsize='8') # for legend title
-   
-    ax.plot(ax.get_xlim(),ax.get_xlim(), color="red", linewidth=0.5)
+ 
+    # one-to-one line
+    ax.plot(ax.get_xlim(),ax.get_xlim(), color=gray, zorder=ax.get_zorder()-1)
     ax.set_aspect('equal')
 
     if label:
         labels = obs[["time","station"]]
-        labels = [f"{time.strftime('%Y-%m-%d')}\n{station}" for time,station in labels.values] 
+        labels = [f"{time.strftime('%Y%m%d')}\n{station}" for time,station in labels.values] 
         for x,y,s in zip(obs[varx],obs[vary],labels):
-            ax.text(x,y,s,fontsize="xx-small", ha="center", va="center_baseline")
+            ax.text(x,y,s,fontsize=4, ha="center", va="center_baseline")
 
     text = f"created {datetime.datetime.now()}"
     fineprint = plt.annotate(text=text, xy=(2,1), xycoords=('figure pixels','figure pixels'), va="bottom", fontsize=6)
     if no_fineprint: fineprint.set_visible(False)
 
-    odir = "/glade/scratch/ahijevyc/trier/VSE"
-    ofile = os.path.join(odir, f"t.scatter.png")
+    odir = "/glade/scratch/ahijevyc/trier/VSE/skewT"
+    svarx = shortstr(varx)
+    svary = shortstr(vary)
+    ofile = os.path.join(odir, f"{svarx}.{svary}.scatter.png")
     plt.tight_layout()
     plt.savefig(ofile,dpi=175)
     logging.info(f"made {os.path.realpath(ofile)}")
 
+
+
+def shortstr(s):
+    # Turn string variable names with units into short strings appropriate for file name
+    i = s.index("[")
+    s = s[:i-1]
+    s = s.replace(" ","_")
+    return s
 
 if __name__ == '__main__':
     main()
